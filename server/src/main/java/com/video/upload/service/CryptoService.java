af7a7e5a3afcbe28359b8ab6111e76c91a3424c9
Igor <zharikovigor97@gmail.com>
2017-09-18 16:51:31 +0300

    added index.html under site: http://localhost:8080/

--- server/src/main/java/com/video/upload/service/CryptoService.java
+++ server/src/main/java/com/video/upload/service/CryptoService.java
@@ -11,25 +11,30 @@ import java.security.NoSuchAlgorithmException;
  */
 @Component
 public class CryptoService {
-    private final MessageDigest messageDigest;
+
+    private String hashMethod;
 
     @Autowired
-    public CryptoService(ServiceConfiguration serviceConfiguration) throws NoSuchAlgorithmException{
-        this(serviceConfiguration.getHashMethod());
+    public CryptoService(ServiceConfiguration serviceConfiguration) {
+        hashMethod = serviceConfiguration.getHashMethod();
     }
 
-    private CryptoService(String alg) throws NoSuchAlgorithmException {
-        messageDigest = MessageDigest.getInstance(alg);
-    }
 
     public String hash(byte[] bytes){
         byte[] byteResult;
-        synchronized (messageDigest) {
-            messageDigest.update(bytes);
-            byteResult = messageDigest.digest();
+        MessageDigest messageDigest = null;
+        try {
+            messageDigest = MessageDigest.getInstance(hashMethod);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
         }
+        if ( messageDigest == null){
+            return null;
+        }
+        messageDigest.update(bytes);
+        byteResult = messageDigest.digest();
         StringBuilder strb = new StringBuilder();
-        for (byte b : byteResult){
+        for (byte b : byteResult) {
             strb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
         }
         return strb.toString();
